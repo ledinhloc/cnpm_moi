07cbb31a42861d844a763e60ff25a8ae57094f2f
ledinhloc <ledinhloc7@gmail.com>
2025-08-20 09:46:29 +0700

    add bt tuan 1 thu 4

---
+++ tuan1/full_stack_01/src/config/configdb.js
@@ -0,0 +1,21 @@
+// src/config/configdb.js
+
+import { Sequelize } from 'sequelize';
+
+// Option 3: Passing parameters separately (other dialects)
+const sequelize = new Sequelize('node_fulltask', 'root', 'root', {
+    host: 'localhost',
+    dialect: 'mysql',
+    logging: false
+});
+
+let connectDB = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
+module.exports = connectDB;
